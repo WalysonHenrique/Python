bb7c84b360a02eabcb960714567b16dc4470b4bc
Walyson Henrique <walysonhenriqueozoriosobrinho@gmail.com>
2025-04-26 00:24:37 -0300

    Trabalho de raspar o olx feito

--- trabalhoRaparOlx.py
+++ trabalhoRaparOlx.py
@@ -7,29 +7,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import os
+os.environ['WDM_LOG_LEVEL'] = '0'
 
 
 
 
-def varrerLinks():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
-    
-    driver = webdriver.Chrome()
+def varrerLinks():    
+    driver = webdriver.Edge()
     
     driver.get('https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-es?ps=10000&pe=10000&exc=1&ms=0&me=5000')
     time.sleep(3)
     html = driver.page_source
-    driver.close()
+    driver.quit()
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    linksCarros = [a.get('href') for a in soup.find_all('a', href=True, class_={'AdCard_link__4c7W6'})]
+    linksCarros = [a.get('href') for a in soup.find_all('a', href=True, class_={'olx-adcard__link'})]
     return linksCarros
 
 
 def varrerPagina(linksCarros):
     
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    anuncio = []
+    anuncios = []
+    
     
   
     
@@ -37,12 +36,12 @@ def varrerPagina(linksCarros):
     countPages = 1
     for linkUrl in linksCarros:
         
-        driver = webdriver.Chrome()
+        driver = webdriver.Edge()
         time.sleep(5)
         driver.get(linkUrl)
         time.sleep(3)
         html = driver.page_source
-        driver.close()
+        driver.quit()
         
         soup = bs4.BeautifulSoup(html, 'html.parser')
         
@@ -66,33 +65,21 @@ def varrerPagina(linksCarros):
             linksDoContainer = link.select('a.olx-link.olx-link--small.olx-link--grey.ad__sc-2h9gkk-3.lkkHCr')
             
         
-        
-        marca = linksDoContainer[2].text
-        modelo = linksDoContainer[1].text
-        ano = linksDoContainer[3].text
-        cambio = spansDoContainer[3].text
-        tipo = spansDoContainer[0].text
-        cor = spansDoContainer[5].text
-        valor = valor.text
-    
+        anuncio = anuncio+[
+            linksDoContainer[2].text,
+            linksDoContainer[1].text,
+            linksDoContainer[3].text,
+            spansDoContainer[3].text,
+            spansDoContainer[0].text,
+            spansDoContainer[5].text,
+            valor.text.replace("R$ ","").replace(".","").strip(),
+            municipio.text
+            ]
+        anuncios = anuncios+[anuncio]
         print(f"\n\n####################### Página {countPages} #######################\n#######################          #######################")
         print(linkUrl)
-        print(f"Marca : {marca}\nModelo: {modelo}\nAno: {ano}\nCambio: {cambio}\nTipo: {tipo}\nCor: {cor}\nValor: {valor}")
-       
         countPages +=1
-        
-            
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
+    return anuncios
+
 os.system('cls')
-#linkCarro = ['https://es.olx.com.br/norte-do-espirito-santo/autos-e-pecas/carros-vans-e-utilitarios/ford-ecosport-xls-1-6-2007-impecavel-1393112810?lis=listing_2020']
-    
-varrerPagina(varrerLinks())
+print(varrerPagina(varrerLinks()))
