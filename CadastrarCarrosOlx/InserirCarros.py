6c1ba84a6d2615183eb306f8c2176d46b28ddf9c
Walyson Henrique <walysonhenriqueozoriosobrinho@gmail.com>
2025-04-26 22:23:25 -0300

    Trabalhos entregues

--- CadastrarCarrosOlx/InserirCarros.py
+++ CadastrarCarrosOlx/InserirCarros.py
@@ -1,59 +1,102 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 import bs4
 import trabalhoRaparOlx
+import fileinput
 
-anuncios = trabalhoRaparOlx.varrerPagina(trabalhoRaparOlx.varrerLinks())
-driver = webdriver.Edge()
-driver.get("http://weka.inf.ufes.br/IFESTP/login.php")
-time.sleep(4)
-elem = driver.find_element(By.NAME, "username")
 
-time.sleep(1)   
-elem.send_keys("WalysonHenrique")
-time.sleep(1)
-elem = driver.find_element(By.NAME, "password")
-time.sleep(1)
-elem.send_keys("447Pirata")
-time.sleep(1)
-for anuncio in anuncios:
+def cadastrarUsuario(nome, email, senha):
+    driver = webdriver.Edge()
+    driver.get("http://weka.inf.ufes.br/IFESTP/index.php")
+    time.sleep(2)
+    elem = driver.find_element(By.NAME, "username")
+    time.sleep(1)   
+    elem.send_keys(nome)
+    elem = driver.find_element(By.NAME, "email")
+    time.sleep(1)
+    elem.send_keys(email)
+    elem = driver.find_element(By.NAME, "password")
+    time.sleep(1)
+    elem.send_keys(senha)
+    elem = driver.find_element(By.NAME, "passwordConfirm")
+    time.sleep(1)
+    elem.send_keys(senha)
+    elam = driver.find_element(By.NAME, "submit")
+    time.sleep(1)
+    elam.click()
+    time.sleep(1)
+    driver.quit()
+
+
+def inserirCarros(url, nome, email, senha):
+    cadastrarUsuario(nome, email, senha)
+    anuncios = trabalhoRaparOlx.varrerPagina(trabalhoRaparOlx.varrerLinks(url))
+    driver = webdriver.Edge()
+    print(anuncios)
+    driver.get("http://weka.inf.ufes.br/IFESTP/login.php")
+    time.sleep(2)
+    elem = driver.find_element(By.NAME, "username")
+    time.sleep(1)   
+    elem.send_keys(nome)
+    elem = driver.find_element(By.NAME, "password")
+    elem.send_keys(senha)
     elem.send_keys(Keys.RETURN)
-    time.sleep(4)
-    elem = driver.find_element(By.XPATH, "/html/body/div/div/div/button")
-    elem.click()
-    elem = driver.find_element(By.NAME, "marca")
-    time.sleep(1)
-    elem.send_keys(anuncio[])
-    time.sleep(1)
-    elem = driver.find_element(By.NAME, "modelo")
-    time.sleep(1)
-    elem.send_keys("F-50")
-    time.sleep(1)
-    elem = driver.find_element(By.NAME, "ano")
-    time.sleep(1)
-    elem.send_keys("2024")
-    time.sleep(1)
-    elem = driver.find_element(By.NAME, "valor")
-    time.sleep(1)
-    elem.send_keys("150.25")
-    time.sleep(1)
-    elem = driver.find_element(By.NAME, "cambioAutomatico")
-    elem.click()
-    time.sleep(1)
-    elem = driver.find_element(By.NAME, "cor")
-    time.sleep(1)
-    elem.send_keys("v")
-    time.sleep(1)
-    elem.send_keys(Keys.RETURN)
-    time.sleep(1)
-    elem = driver.find_element(By.NAME, "municipio")
-    time.sleep(1)
-    elem.send_keys("Sao Paulo")
-    time.sleep(1)
-    elem.send_keys(Keys.RETURN)
-    time.sleep(1)
-    time.sleep(5)
+
+    for anuncio in anuncios:
+        time.sleep(3)
+        elem = driver.find_element(By.XPATH, "/html/body/div/div/div/button")
+        elem.click()
+        elem = driver.find_element(By.NAME, "marca")
+        time.sleep(1)
+        elem.send_keys(anuncio[0])
+        time.sleep(1)
+        elem = driver.find_element(By.NAME, "modelo")
+        time.sleep(1)
+        elem.send_keys(anuncio[1])
+        time.sleep(1)
+        elem = driver.find_element(By.NAME, "ano")
+        time.sleep(1)
+        elem.send_keys(anuncio[2])
+        time.sleep(1)
+        elem = driver.find_element(By.NAME, "cambioAutomatico")
+        if anuncio[3] == "Automático":
+            elem.click()
+        time.sleep(1)
+        
+        if anuncio[4] == "Sedã":
+            elem = driver.find_element(By.ID, "c_sedan")
+            elem.click()
+        elif anuncio[4] == "Hatch":
+            elem = driver.find_element(By.ID, "c_hatch")
+            elem.click() 
+        time.sleep(1)
+        select = Select(driver.find_element(By.NAME, 'cor'))
+        opcoes_dropdown = [option.text for option in select.options]
+
+        if anuncio[5] in opcoes_dropdown:
+            select.select_by_visible_text(anuncio[5])
+        else:
+            select.select_by_visible_text('Outro')
+            print(anuncio[5])
+        time.sleep(1)
+        elem = driver.find_element(By.NAME, "valor")
+        time.sleep(1)
+        elem.send_keys(anuncio[6])
+        elem = driver.find_element(By.NAME, "municipio")
+        time.sleep(1)
+        elem.send_keys(anuncio[7])
+        time.sleep(1)
+        elem.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    driver.quit()
+    print("Carros cadastrados com sucesso")
 
 
+inserirCarros('https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-es?ps=50000&pe=50000&exc=1&ms=0&me=40000&gb=2&gb=1&cac=3&cac=1&cac=4&cac=2&ctp=8&ctp=9&ctp=3',
+              'Walysinn',
+              'walysinn@gmail.com',
+              '12345')
--- TrabalhoRasparOlx/trabalhoRaparOlx.py
+++ TrabalhoRasparOlx/trabalhoRaparOlx.py
@@ -15,7 +15,7 @@ os.environ['WDM_LOG_LEVEL'] = '0'
 def varrerLinks():    
     driver = webdriver.Edge()
     
-    driver.get('https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-es?ps=10000&pe=10000&exc=1&ms=0&me=5000')
+    driver.get('https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-es?ps=30000&pe=60000&ms=0&me=0&gb=2&gb=1&rs=2020&re=2025')
     time.sleep(3)
     html = driver.page_source
     driver.quit()
@@ -28,10 +28,7 @@ def varrerPagina(linksCarros):
     
     anuncios = []
     
-    
-  
-    
-    time.sleep(3)
+    time.sleep(4)
     countPages = 1
     for linkUrl in linksCarros:
         
@@ -51,32 +48,98 @@ def varrerPagina(linksCarros):
             municipio = i.find('a', class_={'olx-link', 'olx-link--caption',' olx-link--grey'})
             
        
-        divPreco = soup.find_all('div', class_={'ad__sc-q5xder-0','dWqdGZ'})
-        
-        for i in divPreco:
-            valor = i.find('span',class_={'olx-text',' olx-text--title-medium','olx-text--block'})
+        valor = soup.select_one("#price-box-container > div.ad__sc-q5xder-1.hoJpM > div:nth-child(1) > span.olx-text.olx-text--title-medium.olx-text--block")
         
 
-        for container in containers:
-            
-            spansDoContainer = container.select('span.ekhFnR')
+        
+        divDosDados = soup.find_all('div', class_={'ad__sc-2h9gkk-1','bdpQSX','olx-d-flex',' olx-ai-flex-start',' olx-fd-column',' olx-flex'})
             
         for link in containers:
             linksDoContainer = link.select('a.olx-link.olx-link--small.olx-link--grey.ad__sc-2h9gkk-3.lkkHCr')
             
-        
-        anuncio = anuncio+[
-            linksDoContainer[2].text,
-            linksDoContainer[1].text,
-            linksDoContainer[3].text,
-            spansDoContainer[3].text,
-            spansDoContainer[0].text,
-            spansDoContainer[5].text,
-            valor.text.replace("R$ ","").replace(".","").strip(),
-            municipio.text
-            ]
+            
+        marcas = []
+        modelos = []
+        anos = []
+        cambios = []
+        tiposVeiculos = []
+        cores = []
+    
+        marca_encontrada = None
+        modelo_encontrado = None
+        ano_encontrado = None
+        cambio_encontrado = None
+        tipoVeiculo_encontrado = None
+        cor_encontrada = None
+   
+        for div in divDosDados:
+            spans = div.find_all('span', class_={'olx-text', 'olx-text--overline', 'olx-text--block', 'olx-mb-0-5', 'olx-color-neutral-120'})
+            
+            for span in spans:
+                if  span.text.strip() == "Marca":
+                    link_marca = div.find('a', class_={'olx-link', 'olx-link--small', 'olx-link--grey', 'ad__sc-2h9gkk-3', 'lkkHCr'})
+                    if link_marca:
+                        marca_encontrada = link_marca.text.strip()
+                    break
+                
+            for span in spans:
+                if  span.text.strip() == "Modelo":
+                    link_modelo = div.find('a', class_={'olx-link', 'olx-link--small', 'olx-link--grey', 'ad__sc-2h9gkk-3', 'lkkHCr'})
+                    if link_modelo:
+                        modelo_encontrado = link_modelo.text.strip()
+                    break  
+                
+            for span in spans:
+                if  span.text.strip() == "Ano":
+                    link_ano = div.find('a', class_={'olx-link', 'olx-link--small', 'olx-link--grey', 'ad__sc-2h9gkk-3', 'lkkHCr'})
+                    if link_ano:
+                        ano_encontrado = link_ano.text.strip()
+                    break 
+                
+            for span in spans:
+                if  span.text.strip() == "Câmbio":
+                    link_cambio = div.find('span', class_={'ad__sc-hj0yqs-0','ekhFnR'})
+                    if link_cambio:
+                        cambio_encontrado = link_cambio.text.strip()
+                    break
+                
+            for span in spans:
+                if  span.text.strip() == "Tipo de veículo":
+                    link_tipoVeiculo = div.find('span', class_={'ad__sc-hj0yqs-0','ekhFnR'})
+                    if link_tipoVeiculo:
+                        tipoVeiculo_encontrado = link_tipoVeiculo.text.strip()
+                    break
+                
+            for span in spans:
+                if  span.text.strip() == "Cor":
+                    link_cor = div.find('span', class_={'ad__sc-hj0yqs-0','ekhFnR'})
+                    if link_cor:
+                        cor_encontrada = link_cor.text.strip()
+                    break
+
+            marcas = [marca_encontrada if marca_encontrada else 'null']
+            modelos = [modelo_encontrado if modelo_encontrado else 'null']
+            anos = [ano_encontrado if ano_encontrado else 'null']
+            cambios = [cambio_encontrado if cambio_encontrado else 'null']
+            tiposVeiculo = [tipoVeiculo_encontrado if tipoVeiculo_encontrado else 'null']
+            cores = [cor_encontrada if cor_encontrada else 'null']
+            
+            
+            for i in range(len(marcas)):
+                anuncio = [
+                    marcas[i],
+                    modelos[i],
+                    anos[i],
+                    cambios[i],
+                    tiposVeiculo[i],
+                    cores[i],
+                    valor.text.replace(".","").replace("R$","").strip(),
+                    municipio.text
+                    ]
         anuncios = anuncios+[anuncio]
         print(f"\n\n####################### Página {countPages} #######################\n#######################          #######################")
         print(linkUrl)
         countPages +=1
     return anuncios
+
+print(varrerPagina(varrerLinks()))
